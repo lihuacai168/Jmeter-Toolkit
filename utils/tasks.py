ae200ab441cf3e7d04ff728e254089e6438ffe7f
rikasai233 <rikasai233@gmail.com>
2025-06-27 12:55:54 +0800

    fix(style): resolve isort import order issue in utils/tasks.py

--- utils/tasks.py
+++ utils/tasks.py
@@ -6,7 +6,7 @@ import subprocess
 import traceback
 from datetime import datetime
 from pathlib import Path
-from typing import Dict, Any
+from typing import Any, Dict
 
 from celery import current_task
 from loguru import logger
